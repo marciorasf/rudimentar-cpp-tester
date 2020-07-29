5c75788ab0d1ce26b06588cc34916a628e1b43ae
Marcio Souza Filho <marciorasf@gmail.com>
2020-07-28 22:15:24 -0300

    add createFolder script in python

---
+++ cppCreateFolder.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+from pathlib import Path
+
+def createFolder(name):
+  if(os.path.isdir(name)):
+    print('folder "'+name+'" already exists')
+    return
+
+  os.mkdir(name)
+
+  cpp_file = name+"/"+name+".cpp"
+  test_input = name+"/test_input.txt"
+  test_expected_output = name+"/test_expected_output.txt"
+
+  Path(cpp_file).touch()
+  Path(test_input).touch()
+  Path(test_expected_output).touch()
+
+name = sys.argv[1]
+createFolder(name)
