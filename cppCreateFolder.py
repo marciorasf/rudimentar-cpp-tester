7c58639c1f616d8ba44a4a1c9897ea5d4da260b3
Marcio Souza Filho <marciorasf@gmail.com>
2020-07-29 10:13:22 -0300

    reorganize and format code

--- cppCreateFolder.py
+++ cppCreateFolder.py
@@ -4,20 +4,22 @@ import sys
 import os
 from pathlib import Path
 
+
 def createFolder(name):
-  if(os.path.isdir(name)):
-    print('folder "'+name+'" already exists')
-    return
+    if(os.path.isdir(name)):
+        print('folder "' + name + '" already exists')
+        return
 
-  os.mkdir(name)
+    os.mkdir(name)
 
-  cpp_file = name+"/"+name+".cpp"
-  test_input = name+"/test_input.txt"
-  test_expected_output = name+"/test_expected_output.txt"
+    cpp_file = name + "/" + name + ".cpp"
+    test_input = name + "/test_input.txt"
+    test_expected_output = name + "/test_expected_output.txt"
 
-  Path(cpp_file).touch()
-  Path(test_input).touch()
-  Path(test_expected_output).touch()
+    Path(cpp_file).touch()
+    Path(test_input).touch()
+    Path(test_expected_output).touch()
+
 
 name = sys.argv[1]
 createFolder(name)
